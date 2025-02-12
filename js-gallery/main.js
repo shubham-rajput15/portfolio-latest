8e3cbbeb1ea34eb7378922ff62e4b68b6e9c65da
shubham-rajput15 <159634684+shubham-rajput15@users.noreply.github.com>
2025-02-12 13:05:46 +0530

    Update main.js

--- js-gallery/main.js
+++ js-gallery/main.js
@@ -21,7 +21,6 @@
 	    animateIn: 'fadeIn',
 	    nav:true,
 	    dots: true,
-	    dotsData: true,
 	    autoplayHoverPause: false,
 	    items: 1,
 	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
@@ -37,7 +36,7 @@
 	      }
 	    }
 		});
-		
+
 	};
 	carousel();
 
