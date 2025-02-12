19b954cab351e9fba988020c78b58ed54687b0f8
shubham-rajput15 <159634684+shubham-rajput15@users.noreply.github.com>
2025-02-12 12:50:11 +0530

    Update main.js

--- js-gallery/main.js
+++ js-gallery/main.js
@@ -21,6 +21,7 @@
 	    animateIn: 'fadeIn',
 	    nav:true,
 	    dots: true,
+	    dotsData: true,
 	    autoplayHoverPause: false,
 	    items: 1,
 	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
