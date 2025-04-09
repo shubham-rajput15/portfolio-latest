65f6c3c3aa2ce693ef9d4f810c58bf88d23ef490
shubham-rajput15 <159634684+shubham-rajput15@users.noreply.github.com>
2025-04-09 13:25:46 +0530

    Update main.js

--- js-circle/main.js
+++ js-circle/main.js
@@ -1,93 +1,47 @@
-// (function($) {
+(function($) {
 
-// 	"use strict";
+	"use strict";
 
-// 	var fullHeight = function() {
+	var fullHeight = function() {
 
-// 		$('.js-fullheight').css('height', $(window).height());
-// 		$(window).resize(function(){
-// 			$('.js-fullheight').css('height', $(window).height());
-// 		});
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
 
-// 	};
-// 	fullHeight();
+	};
+	fullHeight();
 
-// 	var owl = $('.featured-carousel');
+	var owl = $('.featured-carousel');
 
-//   $('.featured-carousel').owlCarousel({
-//   		animateOut: 'fadeOut',
-//       center: false,
-//       items: 1,
-//       loop: true,
-//       stagePadding: 0,
-//       margin: 0,
-//       smartSpeed: 1500,
-//       autoplay: false,
-//       dots: false,
-//       nav: false,
-//       navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
-//   });
+  $('.featured-carousel').owlCarousel({
+  		animateOut: 'fadeOut',
+      center: false,
+      items: 1,
+      loop: true,
+      stagePadding: 0,
+      margin: 0,
+      smartSpeed: 1500,
+      autoplay: false,
+      dots: false,
+      nav: false,
+      navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+  });
 
-//   $('.thumbnail li').each(function(slide_index){
-//       $(this).click(function(e) {
-//           owl.trigger('to.owl.carousel',[slide_index,1500]);
-//           e.preventDefault();
-//       })
-//   })
+  $('.thumbnail li').each(function(slide_index){
+      $(this).click(function(e) {
+          owl.trigger('to.owl.carousel',[slide_index,1500]);
+          e.preventDefault();
+      })
+  })
 
-//   owl.on('changed.owl.carousel', function(event) {
-//       $('.thumbnail li').removeClass('active');
-//       $('.thumbnail li').eq(event.item.index - 2).addClass('active');
-//   })
-
-// })(jQuery);
-
-
-
-(function ($) {
-    "use strict";
-
-    var fullHeight = function() {
-        $('.js-fullheight').css('height', $(window).height());
-        $(window).resize(function(){
-            $('.js-fullheight').css('height', $(window).height());
-        });
-    };
-
-    fullHeight();
-
-    var owl = $('.featured-carousel');
-
-    $('.featured-carousel').owlCarousel({
-        animateOut: 'fadeOut',
-        center: false,
-        items: 1,
-        loop: true,
-        stagePadding: 0,
-        margin: 0,
-        smartSpeed: 1500,
-        autoplay: false,
-        dots: false,
-        nav: false,
-        navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
-    });
-
-    $('.thumbnail li').each(function (slide_index) {
-        $(this).click(function (e) {
-            owl.trigger('to.owl.carousel', [slide_index, 1500]);
-            e.preventDefault();
-        });
-    });
-
-    owl.on('changed.owl.carousel', function (event) {
-        $('.thumbnail li').removeClass('active');
-        
-        // Set active thumbnail for the current slide
-        if (event.item.index - 2 >= 0) {
-            $('.thumbnail li').eq(event.item.index - 2).addClass('active');
-        } else {
-            $('.thumbnail li').eq(0).addClass('active'); // If index is less than 2, set 1st as active
-        }
-    });
+  owl.on('changed.owl.carousel', function(event) {
+      $('.thumbnail li').removeClass('active');
+      $('.thumbnail li').eq(event.item.index - 2).addClass('active');
+  })
 
 })(jQuery);
+
+
+
+
