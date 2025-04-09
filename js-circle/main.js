dc70b7b7f1af09da63d864356ab9e4411f39b5f1
shubham-rajput15 <159634684+shubham-rajput15@users.noreply.github.com>
2025-04-09 13:36:28 +0530

    Update main.js

--- js-circle/main.js
+++ js-circle/main.js
@@ -1,47 +1,113 @@
+// (function($) {
+
+// 	"use strict";
+
+// 	var fullHeight = function() {
+
+// 		$('.js-fullheight').css('height', $(window).height());
+// 		$(window).resize(function(){
+// 			$('.js-fullheight').css('height', $(window).height());
+// 		});
+
+// 	};
+// 	fullHeight();
+
+// 	var owl = $('.featured-carousel');
+
+//   $('.featured-carousel').owlCarousel({
+//   		animateOut: 'fadeOut',
+//       center: false,
+//       items: 1,
+//       loop: true,
+//       stagePadding: 0,
+//       margin: 0,
+//       smartSpeed: 1500,
+//       autoplay: false,
+//       dots: false,
+//       nav: false,
+//       navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+//   });
+
+//   $('.thumbnail li').each(function(slide_index){
+//       $(this).click(function(e) {
+//           owl.trigger('to.owl.carousel',[slide_index,1500]);
+//           e.preventDefault();
+//       })
+//   })
+
+//   owl.on('changed.owl.carousel', function(event) {
+//       $('.thumbnail li').removeClass('active');
+//       $('.thumbnail li').eq(event.item.index - 2).addClass('active');
+//   })
+
+// })(jQuery);
+
 (function($) {
 
-	"use strict";
+  "use strict";
 
-	var fullHeight = function() {
+  var fullHeight = function() {
+    $('.js-fullheight').css('height', $(window).height());
+    $(window).resize(function(){
+      $('.js-fullheight').css('height', $(window).height());
+    });
+  };
+  fullHeight();
 
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
+  // Main featured carousel
+  var owl = $('.featured-carousel');
 
-	};
-	fullHeight();
-
-	var owl = $('.featured-carousel');
-
-  $('.featured-carousel').owlCarousel({
-  		animateOut: 'fadeOut',
-      center: false,
-      items: 1,
-      loop: true,
-      stagePadding: 0,
-      margin: 0,
-      smartSpeed: 1500,
-      autoplay: false,
-      dots: false,
-      nav: false,
-      navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+  owl.owlCarousel({
+    animateOut: 'fadeOut',
+    center: false,
+    items: 1,
+    loop: true,
+    stagePadding: 0,
+    margin: 0,
+    smartSpeed: 1500,
+    autoplay: false,
+    dots: false,
+    nav: false,
+    navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
   });
 
-  $('.thumbnail li').each(function(slide_index){
-      $(this).click(function(e) {
-          owl.trigger('to.owl.carousel',[slide_index,1500]);
-          e.preventDefault();
-      })
-  })
+  // Thumbnail carousel (bottom)
+  var thumbOwl = $('.thumbnail-carousel');
+  thumbOwl.owlCarousel({
+    items: 3,
+    loop: false,
+    margin: 10,
+    dots: false,
+    nav: true,
+    smartSpeed: 800,
+    navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+  });
 
+  // Click event on thumbnail
+  $('.thumbnail li').each(function(slide_index){
+    $(this).click(function(e) {
+      owl.trigger('to.owl.carousel', [slide_index, 1500]);
+      e.preventDefault();
+
+      // Scroll thumbnail carousel to correct group
+      var groupIndex = Math.floor(slide_index / 3);
+      thumbOwl.trigger('to.owl.carousel', [groupIndex, 800]);
+    });
+  });
+
+  // Sync thumbnails on main slider change
   owl.on('changed.owl.carousel', function(event) {
-      $('.thumbnail li').removeClass('active');
-      $('.thumbnail li').eq(event.item.index - 2).addClass('active');
-  })
+    var index = event.item.index - 2;
+    if (index < 0) index = event.item.count + index;
+    
+    $('.thumbnail li').removeClass('active');
+    $('.thumbnail li').eq(index).addClass('active');
+
+    var groupIndex = Math.floor(index / 3);
+    thumbOwl.trigger('to.owl.carousel', [groupIndex, 800]);
+  });
 
 })(jQuery);
-
 
 
 
